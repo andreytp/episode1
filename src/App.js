9267573103375123c6c9c3c2af692487910fc1cb
andriiko <demigod03_blusher@icloud.com>
2023-06-22 21:35:12 -0400

    Using styles using conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-  <div className="App"> 
-  <GetNameComponet name="Andrii" age={50} email="my@andriico.cloud"/>
-  <GetNameComponet name="Julia" age={47} email="julia@andriico.cloud"/>
-  <GetNameComponet name="Vova" age={24} email="vova@andriico.cloud"/> 
+  <div className={styles.App}>
+  <GetNameComponent name="Andrii" age={50} email="my@andriico.cloud"/>
+  <GetNameComponent name="Julia" age={47} email="julia@andriico.cloud"/>
+  <GetNameComponent name="Vova" age={24} email="vova@andriico.cloud"/>
   </div>);
 }
 
-const GetNameComponet = (props) => {
-  return (<div>
+const GetNameComponent = (props) => {
+  return (<div className={styles.name}>
     <h1>{props.name}</h1>
     <h2>{props.age}</h2>
     <h3>{props.email}</h3>
