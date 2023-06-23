1a262585f3b36b26067f3d22eeaf2bc4ee1f5fb6
andriiko <demigod03_blusher@icloud.com>
2023-06-22 20:21:21 -0400

    First component, props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world!</div>
+  return (
+  <div className="App"> 
+  <GetNameComponet name="Andrii" age={50} email="my@andriico.cloud"/>
+  <GetNameComponet name="Julia" age={47} email="julia@andriico.cloud"/>
+  <GetNameComponet name="Vova" age={24} email="vova@andriico.cloud"/> 
+  </div>);
 }
 
+const GetNameComponet = (props) => {
+  return (<div>
+    <h1>{props.name}</h1>
+    <h2>{props.age}</h2>
+    <h3>{props.email}</h3>
+    </div>);
+}
 export default App;
+ 
